2ac0d6687b567f3f51753154e85887a9c2651650
andywar65 <andy.war1965@gmail.com>
2024-04-04 19:09:48 +0200

    simplify drawing create form

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ from djeocadengine.models import Drawing
 class DrawingCreateForm(ModelForm):
     class Meta:
         model = Drawing
-        fields = ["title", "dxf", "temp_image", "image"]
+        fields = ["title", "dxf", "temp_image"]
 
 
 class DrawingParentForm(ModelForm):
